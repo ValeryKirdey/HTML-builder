f3fe0710a8e2e635aba1098c834f14aa8d667c3a
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-05-22 22:54:43 +0300

    feat: add 02-write-file solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,3 +1,25 @@
-// const fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const way = './text.txt';
+const { stdout, stdin } = process;
 
-// fs.writeFile('text.txt', '"Hello мир!"', function(error);
+fs.writeFile(path.join(__dirname, way), '', (err) => {
+    if (err) throw err;
+});
+
+const ws = fs.createWriteStream(path.join(__dirname, way));
+
+stdout.write('‘Sup? Write me something!\n');
+
+process.on('SIGINT', () => {
+    stdout.write('All the best\n');
+    process.exit();
+});
+
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        stdout.write('Have a nice day!\n');
+        process.exit();
+    }
+    ws.write(data);
+});
